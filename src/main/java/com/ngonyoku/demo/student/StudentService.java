d45d87c5e244bca0d834be3f5054fb0f55f70cc4
Ngonyoku <ngonyoku001@gmail.com>
2021-02-19 01:02:14 +0300

    Dependecy injection performed on the StudentService class

--- src/main/java/com/ngonyoku/demo/student/StudentService.java
+++ src/main/java/com/ngonyoku/demo/student/StudentService.java
@@ -1,10 +1,13 @@
 package com.ngonyoku.demo.student;
 
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
 //This is the Business Layer
+@Service //This annotates that this is a service class and is available for dependecy injection
 public class StudentService {
 
     public List<Student> getStudents() {
